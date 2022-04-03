d9305a44a31767e1863580bc179dbb47a42d4ae9
lukmanx <archer.lukman@gmail.com>
2022-04-03 10:46:30 +0700

    initial commit to push - add struct and its function modifier to repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/milkywoosh/gomod_struct_modif
+
+go 1.17
---
+++ gomod_struct_modif.go
@@ -0,0 +1,20 @@
+package gomod_struct_modif
+
+import "fmt"
+
+type TestStruct struct {
+	name   string
+	wealth int64
+}
+
+func (t TestStruct) SetName(name string) {
+	t.name = name
+}
+func (t TestStruct) SetWealth(number int64) {
+	t.wealth = number
+}
+
+func (t TestStruct) ShowData() {
+	fmt.Println(t.name)
+	fmt.Println(t.wealth)
+}
