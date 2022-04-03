050ff41abd22dd154933681d8affebd274c199a6
lukmanx <archer.lukman@gmail.com>
2022-04-03 11:03:21 +0700

    add pointer to enable editing of struct

--- gomod_struct_modif.go
+++ gomod_struct_modif.go
@@ -7,14 +7,14 @@ type TestStruct struct {
 	wealth int64
 }
 
-func (t TestStruct) SetName(name string) {
+func (t *TestStruct) SetName(name string) {
 	t.name = name
 }
-func (t TestStruct) SetWealth(number int64) {
+func (t *TestStruct) SetWealth(number int64) {
 	t.wealth = number
 }
 
-func (t TestStruct) ShowData() {
+func (t *TestStruct) ShowData() {
 	fmt.Println(t.name)
 	fmt.Println(t.wealth)
 }
